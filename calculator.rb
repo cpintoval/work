2b1688f9809ec5dffc0552ce43ffe4cfca8b41ce
carlospinto13 <cpintovalderrama@gmail.com>
2015-08-14 17:37:47 -0400

    Length Finder Done

---
+++ calculator.rb
@@ -0,0 +1,11 @@
+class Calculator
+
+  def add(a, b)
+    a + b
+  end
+
+  def substract(a, b)
+    a - b
+  end
+
+end
---
+++ length_finder.rb
@@ -0,0 +1,11 @@
+def length_finder(input_array)
+  
+  output_array = []
+  
+  input_array.each do |x|
+    output_array << x.length
+  end
+  
+  output_array
+  
+end
